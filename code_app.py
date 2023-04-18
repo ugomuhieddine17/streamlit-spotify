14105e34215ae7047b13d6ba9049cd8ce3423d3f
Ugo Muhieddine <ugo.muhieddine@student-cs.fr>
2023-04-18 17:41:44 +0200

    debugg

--- code_app.py
+++ code_app.py
@@ -348,8 +348,9 @@ with st.sidebar:
 if st.button('Display the predictions'):
     test_data = test_Data_construction(df_select, node_features)
     st.markdown('coucou')
-    # model = torch.load(DATA_PATH + 'best-model_GAT_MLP_TRAINED_normalized.pt')
-    model = torch.load(DATA_PATH + 'best-model_GAT_MLP_PYVIS.pt')
+    model = torch.load(DATA_PATH + 'best-model_GAT_MLP_TRAINED_normalized.pt')
+    print(model)
+    # model = torch.load(DATA_PATH + 'best-model_GAT_MLP_PYVIS.pt')
     test_pred = model(test_data.x, test_data.y_indices)
     proba_featuring = test_pred[:,1].tolist()
     proba_featuring = [round(prob,3) for prob in proba_featuring]
